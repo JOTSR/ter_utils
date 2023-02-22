485202216940f711c5a47fc676a4b9e3515463fc
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-02-22 13:52:00 +0100

    feat: parse cassy csv files into Datas interface

--- deps.ts
+++ deps.ts
@@ -4,3 +4,5 @@ export * as Denum from 'https://deno.land/x/denum@v1.2.0/mod.ts'
 import _PolyFit from 'npm:js-polynomial-regression@0.9.1'
 const PolyFit = _PolyFit.default
 export { PolyFit }
+export * as path from 'https://deno.land/std@0.177.0/path/mod.ts'
+export * as fs from 'https://deno.land/std@0.177.0/fs/mod.ts'
---
+++ src/cassy.ts
@@ -0,0 +1,100 @@
+import { fs, path } from '../deps.ts'
+
+export class Cassy {
+	/**
+	 * Parse cassy text data to Datas interface.
+	 * @param filePath - Path of the file.
+	 * @param  options - name: Name of the measure entry.
+	 * @returns Datas.
+	 */
+	static async read(filePath: string, { name }: { name: string }) {
+		const { base } = path.parse(filePath)
+		const regExp = /"(.+)"\s(\S+)\s?\/?\s?(\S*)/
+
+		const file = await Deno.readTextFile(filePath)
+		const format = file
+			.split('\n')
+			.at(4)
+			?.split('\t')
+			.slice(0, -1)
+			.map((header) => header.match(regExp) ?? [])
+			.map(([_, description, name, unit]) => ({
+				name,
+				unit: unit !== '' ? unit : undefined,
+				description,
+			}))
+
+		if (format === undefined) {
+			throw new TypeError(
+				`can't parse headers from [${
+					file.split('\n').at(4)?.split('\t').slice(0, -1)
+				}]`,
+			)
+		}
+
+		const datas = file
+			.split('\n')
+			.slice(5)
+			.map((row) =>
+				row.split('\t').slice(0, -1).map((value) =>
+					parseFloat(value.replace(',', '.'))
+				)
+			)
+
+		return {
+			conditions: {},
+			measures: [{
+				name,
+				description: base,
+				conditions: [],
+				format,
+				datas,
+			}],
+		} satisfies Datas
+	}
+
+	/**
+	 * Get all datas of a cassy text files matching a glob expression and encapse in one Datas object as a serie of measures.
+	 * @param glob - The glob to expand.
+	 * @param  options - names: Name list for each measures.
+	 * @returns Datas.
+	 */
+	static async readDir(glob: string, { names }: { names: string[] }) {
+		const datas: Datas[] = []
+
+		for await (const file of fs.expandGlob(glob)) {
+			if (file.isFile) {
+				datas.push(
+					await this.read(file.path, { name: names.shift() ?? 'undefined' }),
+				)
+			}
+		}
+
+		return {
+			...datas[0],
+			measures: datas.map(({ measures }) => measures).flat(),
+		} satisfies Datas
+	}
+}
+
+export type Datas = {
+	conditions: Record<string, unknown>
+	measures: {
+		name: string
+		description: string
+		conditions: (Pick<DatasEntry, 'name' | 'value'> & DatasEntry)[]
+		format: (
+			& Pick<DatasEntry, 'name'>
+			& Partial<Pick<DatasEntry, 'unit' | 'uncert' | 'description'>>
+		)[]
+		datas: (string | number)[][]
+	}[]
+}
+
+export type DatasEntry = {
+	name?: string
+	unit?: string
+	value?: string | number
+	uncert?: string | number
+	description?: string
+}
